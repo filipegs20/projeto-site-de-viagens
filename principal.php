eb7dd9232bfac827164766dbac648c1db2cdc5d5
Filipe <filipegabriel132@gmail.com>
2024-01-29 12:45:42 -0300

    atualiação de projeto

--- principal.php
+++ principal.php
@@ -1,9 +1,10 @@
 <div class="container_page">
-
-<div>
+<div >
 	<h1 class="">Viage com a gente</h1>
 </div>
+<p>&nbsp;</p>
 
+<div class="row row-cols-1 row-cols-md-4 g-4">
 <?php 
 $sql_code = "SELECT * from pacotes";    
     
@@ -14,78 +15,35 @@ $sql_query = $mysqli->query($sql_code) or die("falha na execução do código sq
 while ($row = $sql_query->fetch_assoc()) {
     $cards[] = $row; 
 }
-foreach ($cards as $data) {
+
+foreach (array_slice($cards, 0, 3) as $data) {
     
-  //gerando explode nos links do campo imagem para poder selecionar a imagem de capa, que sera a primeira imagem (por padrao).
-  $imagem_de_capa = explode(',',$data['imagem']); 
-  
-  //tratando os pacotes com desconto ou nao.
-  if($data['desconto'] > 0) {
-  
-  //$valorDecimalComPonto = str_replace(',', '.', $data['preco']);
-  //$preco = (float)$valorDecimalComPonto;    
-  $calc = $data['desconto'] * $data['preco'] / 100;
-  $result1 = $data['preco'] - $calc; 
-  $result2 = number_format($result1, 2, ',', '.');
-  $result3 = number_format($data['preco'], 2, ',', '.');
+    //gerando explode nos links do campo imagem para poder selecionar a imagem de capa, que sera a primeira imagem (por padrao).
+    $imagensarray = explode(',', $data['imagem']);
 
-  }
-  $imagensarray = explode(',', $data['imagem']);
-?>
+ ?>
+    
 
-<!-------------------------------------------- carrosel --------------------------------------------------->
-<div style="width: 95%; margin:auto">
-    <div id="carouselExampleCaptions" class="carousel slide" style="max-height: 450px; overflow: hidden;">
-        <div class="carousel-indicators">
-            <?php
-            // Contador para atribuir o indice aos botoes
-            $i = 0;
-
-            foreach ($imagensarray as $carrosel) {
-                // Adiciona a classe "active" apenas ao primeiro botão
-                $activeClass = ($i === 0) ? 'active' : '';
-                
-                // Imprime o botão indicador
-                print '<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="' . $i . '" class="' . $activeClass . '" aria-label="Slide ' . ($i + 1) . '"></button>';
-                
-                $i++;
-            }
-            ?>
+      <a href="?page=pacote&id="<?php print $data['id']; ?>>
+        <div class="col">
+          <div class="card h-100">
+            <img src="<?php print $imagensarray['0'];?>" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title"><?php print $data['nome_pacote']; ?></h5>
+              <p class="card-text"><?php print $data['descricao']; ?></p>
+            </div>
+          </div>
         </div>
+      </a>
 
-        <div class="carousel-inner">
-            <?php
-            // Reiniciando o contador para atribuir a classe "active" apenas ao primeiro slide
-            $i = 0;
-
-            foreach ($imagensarray as $carrosel) {
-                // Adiciona a classe "active" apenas ao primeiro slide
-                $activeClass = ($i === 0) ? 'active' : '';
-                ?>
-                <div class="carousel-item <?php print $activeClass; ?>">
-                    <img src="<?php print $imagem_de_capa['0']; ?>"
-                         class="d-block w-100 mx-auto img-fluid" style="height: 400px; object-fit: contain;" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                    </div>
-                </div>
-                <?php
-                $i++;
-            }
-            ?>
-        </div>
-
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Anterior</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Próximo</span>
-        </button>
+<?php } ?>
+</div>
+<!-- <div class="card" style="width: 18rem; ">
+    <img src="https://media.istockphoto.com/id/1166728645/pt/foto/suspension-bridge-cable-stayed-bridge-in-the-world.jpg?s=612x612&w=0&k=20&c=_2bO5TdCKNzrSHgaofCvqpQRiwRtNtFOLdeI8WpsMBc=" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
-</div>
+  </div> -->
 
-
-<?php }?>
-
-</div>
+</body>
+</html>
